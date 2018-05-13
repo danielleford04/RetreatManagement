a9852a6e266bcb416c47600cad7f87e303dceda5
danielleford <dford@phizzle.com>
2018-05-12 19:16:23 -0600

    first call wired in successfully - retreatants get

--- src/reducers/reducer_retreatants.js
+++ src/reducers/reducer_retreatants.js
@@ -13,7 +13,7 @@ import { FETCH_RETREATANTS } from '../actions/index';
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_RETREATANTS:
-      return _.mapKeys(action.payload.data, 'id');
+      return _.mapKeys(action.payload.data, '_id');
     default:
       return state;
   }
